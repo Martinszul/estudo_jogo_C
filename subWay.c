87b1f61e36e9d335a1a14f0093d7c17bdb573660
gabriel-landim1 <gabriellandimmarcelino@gmail.com>
2026-05-06 21:09:47 -0300

    Update subWay.c

--- subWay.c
+++ subWay.c
@@ -1,17 +1,21 @@
 #include <GL/freeglut.h>
-#include <GL/glut.h>
+#include <stdlib.h>
+#include <time.h>
 
 // JOGADOR
 int moveEsq, moveDir, moveCima, moveBaixo;
 float xPOS, yPOS;
 
 //obstaculo desce
-float pos = 1;
+float posY = 1.5;
+float posX = 0.0;
 
 // MAPA
 float descerMapa = 0.0;
 float tamanhoX = 0.056;
 float tamanhoY = 0.08;
+float limiteX = 0.6;
+float limiteY = 0.84;
 
 //IMAGEM DO CARRO JOGADORORR
 int carrinho[20][10] = {
@@ -34,7 +38,6 @@ int carrinho[20][10] = {
     {1, 2, 2, 2, 1, 1, 2, 2, 2, 1},
     {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
     {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
-
 };
 
 //COR DO CARRO JOGADOR
@@ -44,7 +47,7 @@ void carrinhoRodar(){
               if(carrinho[l][c] != 0){
                  if(carrinho[l][c] == 1) glColor3ub(0, 0, 0); if(carrinho[l][c] == 2) glColor3ub(255,0,0); if(carrinho[l][c] == 3) glColor3ub(255,255,255);
             
-                 float x = c * 0.0090 - 0.09; float y = -l * 0.0090 - 0.011;
+                 float x = c * 0.0090 - 0.045; float y = -l * 0.0090 - 0.011;
 
                 glBegin(GL_QUADS);
                     glVertex2f(x, y);
@@ -107,28 +110,29 @@ void estrada1Rodar(){
         }
     }
 }
+
 //obstaculo 1
 int obstaculo1[20][10] = {
-    {0, 0, 0, 2, 2, 2, 2, 0, 0, 0},
-    {0, 0, 2, 2, 2, 2, 2, 2, 0, 0},
-    {0, 2, 3, 3, 2, 2, 3, 3, 2, 0},
-    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
-    {1, 2, 2, 1, 1, 1, 1, 2, 2, 1},
-    {1, 2, 1, 1, 1, 1, 1, 1, 2, 1},
-    {0, 2, 1, 1, 1, 1, 1, 1, 2, 0},
-    {0, 2, 1, 3, 2, 2, 3, 1, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
-    {1, 2, 2, 3, 2, 2, 3, 2, 2, 1},
-    {1, 2, 2, 1, 1, 1, 1, 2, 2, 1},
+    {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
+    {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
     {1, 2, 2, 2, 1, 1, 2, 2, 2, 1},
-    {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
-    {0, 2, 2, 2, 2, 2, 2, 2, 2, 0},
-
+    {1, 2, 2, 1, 1, 1, 1, 2, 2, 1},
+    {1, 2, 2, 3, 2, 2, 3, 2, 2, 1},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 2, 3, 2, 2, 3, 2, 2, 0},
+    {0, 2, 1, 3, 2, 2, 3, 1, 2, 0},
+    {0, 2, 1, 1, 1, 1, 1, 1, 2, 0},
+    {1, 2, 1, 1, 1, 1, 1, 1, 2, 1},
+    {1, 2, 2, 1, 1, 1, 1, 2, 2, 1},
+    {1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+    {0, 2, 3, 3, 2, 2, 3, 3, 2, 0},
+    {0, 0, 2, 2, 2, 2, 2, 2, 0, 0},
+    {0, 0, 0, 2, 2, 2, 2, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
 //COR DO obstaculo
@@ -153,14 +157,15 @@ void obstaculo1Rodar(){
 
 //fazer obstaculo descer
 void update(int value){
+    
+    posY -= 0.01;
 
-    pos -= 0.01;
-
-    if(pos < -1.1)
-        pos = 1;
-
+    if(posY < -1.1){
+        posY = 2.0;
+        posX = ((rand() % 125) / 100.0) - 0.65;
+    }
     glutPostRedisplay();
-    glutTimerFunc(16,update,0);
+    glutTimerFunc(8,update,0);
 }
 
 
@@ -184,6 +189,11 @@ void caminhar(int valor){
     if(moveDir) xPOS += velocidadeCaminhar; 
     if(moveCima) yPOS += velocidadeCaminhar;
     if(moveBaixo) yPOS -= velocidadeCaminhar;
+
+    if(xPOS > limiteX) xPOS = limiteX;
+    if(xPOS < -limiteX) xPOS = -limiteX;
+    if(yPOS < -limiteY) yPOS = -limiteY;
+    if(yPOS > limiteY) yPOS = limiteY;
     
     glutTimerFunc(2, caminhar, 0);
     glutPostRedisplay();
@@ -197,7 +207,7 @@ void display(){
     carrinhoRodar();
     glPopMatrix();
     glPushMatrix();
-    glTranslatef(0,pos,0);
+    glTranslatef(posX,posY,0);
     obstaculo1Rodar();
     glPopMatrix();
     glFlush();
@@ -205,6 +215,7 @@ void display(){
 
 int main(int argc, char** argv){
     glutInit(&argc, argv);
+    srand(time(NULL));
     
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     
